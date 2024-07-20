e0c3a6fbb158a468e505e3fd1c6669286f1da848
dotmv404 <grasslover045@gmail.com>
2024-07-21 04:44:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -68,22 +68,85 @@ client.on("messageCreate", async (message) => {
 
 
   if (message.author.bot) return;
-  if (!message.content.includes("<@1155782105352114236>")) return;
+  if (message.content.startsWith("dotvc")){
 
+    let getchanneldb = (db.get(message.channel.id) || {}).bot ?? false;
+
+    console.log(getchanneldb)
+    
+    
+      if (getchanneldb === true) {
+        const kafachannels = "1264361420871569511";
+        const kafabotchannel = client.channels.cache.get(kafachannels);
+        if (message.author.id === "1155782105352114236") return;
+    
+        if (kafabotchannel) {
+          let amessageedit = message.content.replace(
+            "dotvc",
+            " "
+          );
+          kafabotchannel.send(amessageedit);
+          console.log(message.content);
+    
+          const filter = (response) => response.author.id === "1214097446263857192";
+    
+          kafabotchannel
+            .awaitMessages({ filter, max: 1, time: 60000, errors: ["time"] })
+            .then((collected) => {
+              const reply = collected.first();
+              if (reply) {
+                let username = message.author.username;
+                let displayName = message.member
+                  ? message.member.displayName
+                  : username;
+                console.log(reply.content);
+                // message.reply(
+                //   `${reply.content.replace("dotingmywholelife", displayName)}`
+                // );
+    
+                const channel = client.channels.cache.get(message.channel.id);
+                const attachment = new MessageAttachment(
+                  reply.content, // path file
+                  'random_file_name.ogg', // must be .ogg
+                  {
+                    waveform: 'AAAAAAAAAAAA',
+                    duration_secs: 1, // any number you want
+                  },
+                );
+                channel.send({
+                  files: [attachment],
+                  flags: 'IS_VOICE_MESSAGE',
+                });
+    
+    
+    
+    
+    
+    
+    
+              }
+            })
+            .catch(() => {
+              console.log("No reply received within the time limit.");
+            });
+        } else {
+          console.error("Channel not found!");
+        }
+      }
+    
+
+  }
+
+
+
+
+
+
+
+  if (!message.content.includes("<@1155782105352114236>")) return;
   let getchanneldb = (db.get(message.channel.id) || {}).bot ?? false;
 
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(getchanneldb)
   if (getchanneldb === true) {
     const dotbotchannels = "1264293369010589808";
     const dotbotchannel = client.channels.cache.get(dotbotchannels);
